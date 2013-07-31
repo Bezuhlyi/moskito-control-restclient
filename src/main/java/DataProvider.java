d4b07ebf480f0b39f60cb9f1e8206eadee8fc0e7
bezuhlyi <vbezuhlyi@anotheria.net>
2013-07-31 21:31:56 +0300

    Maven support.

---
+++ src/main/java/DataProvider.java
@@ -0,0 +1,16 @@
+/**
+ * Instance that can be asked to provide data from specified running MoSKito Control application.
+ *
+ * @author Vladyslav Bezuhlyi
+ */
+public class DataProvider {
+
+	private static DataProvider instance = new DataProvider();
+
+	public static DataProvider getInstance() {
+		return instance;
+	}
+
+	private DataProvider() {
+	}
+}
